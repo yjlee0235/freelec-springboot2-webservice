561572f69f288a1461c0fd5ee38b836c94f80da9
yjlee0235 <yjlee0235@naver.com>
2022-12-15 21:20:08 +0900

    fix: deploy.sh를 통해 현재 구동 중인 애플리케이션의 PID를 못가져오는 문제 해결

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,14 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl demo | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "kill -15 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
@@ -29,7 +29,7 @@ echo "> $JAR_NAME 에 실행 권한 추가"
 
 chmod +x $JAR_NAME
 
-echo "> $JAR_Name 실행"
+echo "> $JAR_NAME 실행"
 
 nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
